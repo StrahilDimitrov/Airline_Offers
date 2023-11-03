6f68efcd141fc73c674c653df4867e5b15cad1ef
Strahil Hristov <strahil1998@abv.bg>
2023-11-03 16:26:06 +0200

    components

--- airline/src/Components/Lounge/Lounge.jsx
+++ airline/src/Components/Lounge/Lounge.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import loungePics from '../../assets/lounge.jpg';
 
 const Lounge = () => {
-    return(
+    return (
         <div className="lounge container section">
             <div className="sectionContainer grid">
 
@@ -12,13 +12,47 @@ const Lounge = () => {
 
                 <div className="textDiv">
                     <h2>Unaccompanied Minor Lounge</h2>
-                </div>
+                    <div className="grids grid">
 
-                <div className="grids grid">
-                    <div className="singleGrid">
-                        <span className="gridTitle">
-                            Help through the airport
-                        </span>
+                        <div className="singleGrid">
+                            <span className="gridTitle">
+                                Help through the airportds
+                            </span>
+                            <p className="paragraphe">
+                                You can also call airlines from your phone and book a flight ticket
+                                to one of your favorite destinations.
+                            </p>
+                        </div>
+
+                        <div className="singleGrid">
+                            <span className="gridTitle">
+                                Priority Boarding
+                            </span>
+                            <p className="paragraphe">
+                                You can also call airlines from your phone and book a flight ticket
+                                to one of your favorite destinations.
+                            </p>
+                        </div>
+
+                        <div className="singleGrid">
+                            <span className="gridTitle">
+                                Care on the flight
+                            </span>
+                            <p className="paragraphe">
+                                You can also call airlines from your phone and book a flight ticket
+                                to one of your favorite destinations.
+                            </p>
+                        </div>
+
+                        <div className="singleGrid">
+                            <span className="gridTitle">
+                                Chauffeur-drive service
+                            </span>
+                            <p className="paragraphe">
+                                You can also call airlines from your phone and book a flight ticket
+                                to one of your favorite destinations.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
--- airline/src/Components/Travelers/Travelers.jsx
+++ airline/src/Components/Travelers/Travelers.jsx
@@ -1,8 +1,80 @@
 import React from "react";
+// Import destination picture
+import moscow from '../../assets/moscow.jpg';
+import dubai from '../../assets/dubai.jpg';
+import abuDhabi from '../../assets/abuDhabi.jpg';
+import paris from '../../assets/paris.jpg';
 
+// Import user pics
+import user1 from '../../assets/user(1).jpg';
+import user2 from '../../assets/user(2).jpg';
+import user3 from '../../assets/user(3).jpg';
+import user4 from '../../assets/user(4).jpg';
+
+const travelers = [
+    {
+        id: 1,
+        destinationImg: moscow,
+        travelerImg: user1,
+        travelerName: 'AmirMuhammad',
+        socialLink: '@amirmuhammad12'
+    },
+    {
+        id: 2,
+        destinationImg: dubai,
+        travelerImg: user2,
+        travelerName: 'Sasha Mehmedova',
+        socialLink: '@sasha_21'
+    },
+    {
+        id: 3,
+        destinationImg: abuDhabi,
+        travelerImg: user3,
+        travelerName: 'Anastasia Goranova',
+        socialLink: '@goranova002'
+    },
+    {
+        id: 4,
+        destinationImg: paris,
+        travelerImg: user4,
+        travelerName: 'Alexsander Ivanov',
+        socialLink: '@sasho_bg'
+    }
+]
 const Travelers = () => {
-    return(
-        <div>Travelers</div>
+    return (
+        <div className="travelers container section">
+            <div className="sectionContainer">
+                <h2>Top travelers of this month!</h2>
+
+                <div className="travelersContainer grid">
+                    {
+                        travelers.map(({
+                                           id,
+                                           destinationImg,
+                                           travelerImg,
+                                           travelerName,
+                                           socialLink
+                                       }) => {
+                            return (
+                                <div key={id} className="singleTravelers">
+                                    <img src={destinationImg} className="destinationImg"/>
+                                    <div className="travelersDetails">
+                                        <div className="travelersPics">
+                                            <img src={travelerImg} className="travelerImg"/>
+                                        </div>
+                                        <div className="travelerName">
+                                            <span>{travelerName}</span>
+                                            <p>{socialLink}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 

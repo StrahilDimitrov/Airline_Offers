0e027f3f85522e080fc85b0fe9de88508fb27ca1
Strahil Hristov <strahil1998@abv.bg>
2023-11-06 16:20:44 +0200

    component

--- airline/src/Components/Subscribers/Subscribers.jsx
+++ airline/src/Components/Subscribers/Subscribers.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const Subscribers = () => {
     return(
-        <div>Subscribers</div>
+        <div className="subscribe section">
+            <div className="sectionContainer container">
+                <h2>Subscribe to us and see the last news.</h2>
+                <div className="inputDiv flex">
+                    <input type="text" placeholder="Enter your emil..." />
+                    <button className="btn">Subscribe</button>
+                </div>
+            </div>
+        </div>
     )
 }
 

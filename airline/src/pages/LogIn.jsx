3d5fb5df307a0848159a96331d93a2f01d45d7b6
Strahil Hristov <strahil1998@abv.bg>
2023-11-07 12:24:16 +0200

    redact

---
+++ airline/src/pages/LogIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogIn = () => {
+    return(
+        <div>
+            <h1>Log In Form</h1>
+            <input type="text" placeholder="Username..." />
+        </div>
+    )
+}
+
+export default LogIn;
